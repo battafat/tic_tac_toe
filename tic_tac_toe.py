b9f2276f6304e222bd9239f8127330936a755a86
timothy battafarano <tim@deepsprings.edu>
2022-10-03 15:22:06 -0400

    add function to check for winner

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -47,7 +47,81 @@ blank = "__"
 
 #def make_move()
 
+#create a function that takes the board as an argument and then returns either a
+#string of the winner or false
 
+    #Below Determines if someone won.
+    #Create lists of all the 3-item win possibilities:
+    #3 horizontal, 3 vertical, and 2 diagonal.
+    #Then fill those lists with the moves recorded in board. When any of those
+    #lists contains all items equal to themselves, someone has won.
+
+def find_winner(board):
+    # if winner, return winner
+    # else: rreturn faslse
+    horizontal_wins = []
+
+    for row in board:
+        horizontal_wins.append(row)
+
+    #create vertical win lists
+    vertical_wins = []
+    x = 0
+
+
+    for i in range(len(board)):
+    #this repeats the below for loop the number of times that board is long.
+    #3 in this case. This works because the number of rows and columns is equal.
+        column_vertical_win = list()
+        vertical_wins.append(column_vertical_win)
+    #Above, an empty list -- column_vertical_win -- was created and appended above to vertical_wins.
+    #Below, for each row in board:
+    #   starting with the initial list/column in vertical_wins,
+    #   and the initial index in the row of board
+    #   the values from that index of board are appended to the corresponding list/column.
+        for row in board:
+            vertical_wins[x].append(row[x])
+
+        x = x+1
+    #this increases the value of x, or the value of the index, by 1 each time
+
+    #This creates the diagonal win possibilities, with reference to board.
+
+    diagonal_topleft = [board[0][0], board[1][1], board[2][2]]
+
+    diagonal_btmleft = [board[0][2], board[1][1], board[2][0]]
+
+    diagonal_wins = [diagonal_topleft, diagonal_btmleft]
+
+    #Below creates a master list of win lists.
+
+    winner_master = []
+    for row in horizontal_wins:
+        winner_master.append(row)
+
+    for column in vertical_wins:
+        winner_master.append(column)
+
+    for dgnl in diagonal_wins:
+        winner_master.append(dgnl)
+
+    #The below goes through the possible win lists in winner_master.
+    #first: does the list contain blank positions? In which case it cannot be a win.
+    #second: if it doesn't contain blank -- ie, all moves have been made in that possible
+    #       win -- are all of those moves the same?
+    for win in winner_master:
+        #first: does the list contain blank positions? In which case it cannot be a win.
+        if win[0] != blank:
+        #second: if it doesn't contain blank -- ie, all moves have been made in that possible
+        #       win list -- are all of those moves equal? (all X or all O)
+            if win.count(win[0]) == len(win):
+                print(" ")
+        #This prints the winner by simply checking which character occupies the
+        #the initial index in the winning list.
+                return win[0]
+    return False
+
+#has_winner = find_winner(board)
         #check which element (X or O) it contains and declare that person a winner
 
 #The line below checks all the items in each list of coordinates to see if any
@@ -55,10 +129,14 @@ blank = "__"
 positions = [item for sublist in board for item in sublist]
 
 
+
 while blank in positions:
 
+    announce_winner = find_winner(board)
+    print("line 132, announce_winner", announce_winner)
+
     for i in range(3):
-        print(" ")
+            print(" ")
 
 #X makes their move.
 
@@ -91,76 +169,11 @@ while blank in positions:
                     for lst in board:
                         print(lst)
 
+    announce_winner = find_winner(board)
+    print("line 132, announce_winner", announce_winner)
+    if type(announce_winner) == str:
+        break
 
-    #Below Determines if someone won.
-    #Create lists of all the 3-item win possibilities:
-    #3 horizontal, 3 vertical, and 2 diagonal.
-    #Then fill those lists with the moves recorded in board. When any of those
-    #lists contains all items equal to themselves, someone has won.
-
-    horizontal_wins = []
-
-    for row in board:
-        horizontal_wins.append(row)
-
-#create vertical win lists
-    vertical_wins = []
-    x = 0
-
-
-    for i in range(len(board)):
-    #this repeats the below for loop the number of times that board is long.
-    #3 in this case. This works because the number of rows and columns is equal.
-        column_vertical_win = list()
-        vertical_wins.append(column_vertical_win)
-#Above, an empty list -- column_vertical_win -- was created and appended above to vertical_wins.
-#Below, for each row in board:
-#   starting with the initial list/column in vertical_wins,
-#   and the initial index in the row of board
-#   the values from that index of board are appended to the corresponding list/column.
-        for row in board:
-            vertical_wins[x].append(row[x])
-
-        x = x+1
-    #this increases the value of x, or the value of the index, by 1 each time
-
-#This creates the diagonal win possibilities, with reference to board.
-
-    diagonal_topleft = [board[0][0], board[1][1], board[2][2]]
-
-    diagonal_btmleft = [board[0][2], board[1][1], board[2][0]]
-
-    diagonal_wins = [diagonal_topleft, diagonal_btmleft]
-
-#Below creates a master list of win lists.
-
-    winner_master = []
-    for row in horizontal_wins:
-        winner_master.append(row)
-
-    for column in vertical_wins:
-        winner_master.append(column)
-
-    for dgnl in diagonal_wins:
-        winner_master.append(dgnl)
-
-#The below goes through the possible win lists in winner_master.
-#first: does the list contain blank positions? In which case it cannot be a win.
-#second: if it doesn't contain blank -- ie, all moves have been made in that possible
-#       win -- are all of those moves the same?
-    for win in winner_master:
-        #first: does the list contain blank positions? In which case it cannot be a win.
-        if win[0] != blank:
-        #second: if it doesn't contain blank -- ie, all moves have been made in that possible
-        #       win list -- are all of those moves equal? (all X or all O)
-            if win.count(win[0]) == len(win):
-                print(" ")
-        #This prints the winner by simply checking which character occupies the
-        #the initial index in the winning list.
-                print(win[0], "wins!")
-                #At this point I want to break out of the While loop, but I haven't
-                #figured out a way to do so yet.
-                #break
 
 
     #O's Turn:
@@ -193,62 +206,11 @@ while blank in positions:
                         print(lst)
     #print("line 210, vertical_wins", vertical_wins)
 
-    horizontal_wins = []
+    announce_winner = find_winner(board)
+    print("line 132, announce_winner", announce_winner)
+    if type(announce_winner) == str:
+        break
 
-    for row in board:
-        horizontal_wins.append(row)
-
-    #print("line 51, horizontal_wins", horizontal_wins)
-
-    vertical_wins = []
-    x = 0
-
-
-    for i in range(len(board)):
-    #this repeats the below for loop the number of times that board is long. 3 in this case.
-        column_vertical_win = list()
-        vertical_wins.append(column_vertical_win)
-        for row in board:
-        #for each of the rows in board, so 3 times in this case. It goes to each row.
-        #    column_vertical_win.append(row[x])
-            #This appends the initial index of the row it's on to column_vertical_win
-
-            vertical_wins[x].append(row[x])
-
-            #this appends column_vertical_win to vertical_wins
-
-
-        x = x+1
-    #this increases the value of x, or the value of the index, by 1
-
-
-
-
-    diagonal_topleft = [board[0][0], board[1][1], board[2][2]]
-    #print("diagonal_topleft, line 76", diagonal_topleft)
-    diagonal_btmleft = [board[0][2], board[1][1], board[2][0]]
-    #print("diagonal_btmleft, line 78", diagonal_btmleft)
-
-    diagonal_wins = [diagonal_topleft, diagonal_btmleft]
-
-
-    winner_master = []
-    for row in horizontal_wins:
-        winner_master.append(row)
-
-    for column in vertical_wins:
-        winner_master.append(column)
-
-    for dgnl in diagonal_wins:
-        winner_master.append(dgnl)
-
-    for win in winner_master:
-        if win[0] != blank:
-            if win.count(win[0]) == len(win):
-                print("line 264, winner_master", winner_master)
-                print(" ")
-                print(win[0], "wins!")
-                break
 
     for i in range(3):
         print(" ")
@@ -261,6 +223,7 @@ else:
 
 #What happens if the user inputs an unuseable format or a space that's already taken?
 
+winner = find_winner(board)
 
 
 #Maybe there’s a possibility that I could associate all the number with their
